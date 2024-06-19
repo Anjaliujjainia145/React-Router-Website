a7fd6cb427d93df450e02baa112901473024c944
Anjaliujjainia145 <152694177+Anjaliujjainia145@users.noreply.github.com>
2024-06-19 10:31:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+
+function App() {
+  
+
+  return (
+    <>
+    {/* <Header></Header> */}
+    {/* <Footer></Footer> */}
+    {/* <Home></Home> */}
+      
+      
+    </>
+  )
+}
+
+export default App
